c51d949c3ad59055f09e4835956a5f676b3d03a0
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-09-06 16:02:02 -0700

    Adding time unit constants and testing that they remain what the tester expects.
    Signed-off-by: Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>

--- lib/tardis.rb
+++ lib/tardis.rb
@@ -1,5 +1,20 @@
-require "tardis/version"
+require_relative 'tardis/version'
+require_relative 'tardis/math'
+require_relative 'tardis/convert'
 
 module Tardis
-  # Your code goes here...
+  Second     = 1
+  Minute     = Second  * 60
+  Hour       = Minute  * 60
+  Day        = Hour    * 24
+  Week       = Day     * 7
+  Month      = Week    * 4
+  Year       = Month   * 12
+  Decade     = Year    * 10
+  Century    = Decade  * 10
+end
+
+class Fixnum
+  include Tardis::Convert
+  include Tardis::Math
 end

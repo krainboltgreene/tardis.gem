f48307f18e9e31acf04883782f4e03ce79f6c618
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-10-02 08:33:05 -0500

    Refactoring with metaprogramming.
    Signed-off-by: Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>

--- lib/tardis/time/math.rb
+++ lib/tardis/time/math.rb
@@ -1,78 +1,18 @@
 module Tardis::Time::Math
-  def second
-    self == 1 ? Tardis::Time::Second : self * Tardis::Time::Second
+  units = %w[second minute hour day week month year decade century]
+  units.each do |u|
+    self.send :define_method, u do
+      unit = Tardis::Time.const_get(u.capitalize.to_sym)
+      self == 1 ? unit : self * unit
+    end
   end
   alias_method :seconds, :second
-
-  def minute
-    if self == 1
-      Tardis::Time::Minute
-    else
-      self * Tardis::Time::Minute
-    end
-  end
   alias_method :minutes, :minute
-
-  def hour
-    if self == 1
-      Tardis::Time::Hour
-    else
-      self * Tardis::Time::Hour
-    end
-  end
   alias_method :hours, :hour
-
-  def day
-    if self == 1
-      Tardis::Time::Day
-    else
-      self * Tardis::Time::Day
-    end
-  end
   alias_method :days, :day
-
-  def week
-    if self == 1
-      Tardis::Time::Week
-    else
-      self * Tardis::Time::Week
-    end
-  end
   alias_method :weeks, :week
-
-  def month
-    if self == 1
-      Tardis::Time::Month
-    else
-      self * Tardis::Time::Month
-    end
-  end
   alias_method :months, :month
-
-  def year
-    if self == 1
-      Tardis::Time::Year
-    else
-      self * Tardis::Time::Year
-    end
-  end
   alias_method :years, :year
-
-  def decade
-    if self == 1
-      Tardis::Time::Decade
-    else
-      self * Tardis::Time::Decade
-    end
-  end
   alias_method :decades, :decade
-
-  def century
-    if self == 1
-      Tardis::Time::Century
-    else
-      self * Tardis::Time::Century
-    end
-  end
   alias_method :centuries, :century
 end

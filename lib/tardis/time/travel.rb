1554669c9a7baf21635091b426a790d4ff32619c
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-10-02 07:50:17 -0500

    Adding propper documentation to .from() and .to()
    Signed-off-by: Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>

--- lib/tardis/time/travel.rb
+++ lib/tardis/time/travel.rb
@@ -3,11 +3,22 @@ module Tardis::Time::Travel
   # It takes one argument, the a time object or Integer representing seconds.
   # The returned object is an Integer which represents a number of seconds
   # between that point in time and now.
+
+
+  # example: time_left = next_birthday.from Date.yesterday
+  # anatomy: [Time|Date].from([Time|Date]) => Integer
+  # description: |
+  #   Called on a Time or Date object and given a Time or Date object.
+  #   Returns an Integer, representing a number of seconds between the
+  #   object called on (a time in the future) and the object given (a
+  #   time in the past).
   def from(timestamp)
-    # Transforming the time/date object (self) to an integer.
+    # Transform the time/date object (self), which represents the
+    # starting point, to an integer.
     beginning = self.to_i
 
-    # Transforming the time/date object given to an integer.
+    # Transform the time/date object (an argument), which represents
+    # the point in the
     ending = timestamp.to_i
 
     # Subtract the end period from the start period which will
@@ -16,17 +27,28 @@ module Tardis::Time::Travel
     beginning - ending
   end
 
-  # The .to() method is called on a Time or Date object.
-  # It takes one argument, the a time object or Integer representing seconds.
-  # The returned object is an Integer which represents a number of seconds
-  # between that point in time and now
+  # example: @account.created_at.to Date.today
+  # anatomy: [Time|Date].from([Time|Date]) => Integer
+  # description: |
+  #   Called on a Time or Date object and given a Time or Date object.
+  #   Returns an Integer, representing a number of seconds between the
+  #   object called on (a time in the past) and the object given (a
+  #   time in the future).
   def to(timestamp)
-    timestamp.to_i - self.to_i
+    # Transform the time/date object (self), which represents the
+    # starting point, to an integer.
+    beginning = timestamp.to_i
+
+    # Transform the time/date object (an argument), which represents
+    # the point in the
+    ending = self.to_i
+
+    # Subtract the end period from the start period which will
+    # return the total amount of time in seconds from the
+    # start the end.
+    beginning - ending
   end
 
-  # The .to_now() method is called on a Time or Date object.
-  # The returned object is an Integer which represents a number of seconds
-  # between that point in time and now.
   def from_now
     self.from Time.new
   end
